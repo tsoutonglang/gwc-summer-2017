dc91db642eabf398693e26f87b38ad53ec303620
tsoutonglang <tsoutonglang12@gmail.com>
2017-07-06 13:54:14 -0500

    first code input

---
+++ JabaTheHut.py
@@ -0,0 +1,44 @@
+from turtle import *
+import math
+
+# Name your Turtle.
+t = Turtle()
+
+# Set Up your screen and starting position.
+setup(500,300)
+x_pos = -250
+y_pos = -150
+#t.setposition(x_pos, y_pos)
+
+### Write your code below:
+t.pencolor("rosy brown")
+#for BountyHunters in range(3):
+t.forward(75)
+t.left(140)
+t.forward(50)
+t.left(80)
+t.forward(50)
+t.penup()
+t.right(-50)
+t.pendown()
+t.pencolor("aquamarine")
+#for BountyHunters in range(3):
+t.forward(75)
+t.left(90)
+t.forward(25)
+t.left(90)
+t.pencolor("coral")
+for BountyHunters in range(2):
+    t.forward(30)
+    t.right(90)
+    t.forward(25)
+    t.right(90)
+t.right(90)
+t.forward(25)
+t.pencolor("aquamarine")
+t.forward(25)
+t.left(90)
+t.forward(75)
+
+# Close window on click.
+exitonclick()
